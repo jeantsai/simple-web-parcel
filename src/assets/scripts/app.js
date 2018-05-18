bbaa8a339f56ce12b604ceae7e7a521e391c2fe5
jeantsai <jeantsai@qq.com>
2018-05-18 16:08:53 +0800

    Fix a issue which cause .ready cannot work normally

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -1,4 +1,4 @@
 $(document).ready(function () {
     document.getElementById('simple-main').innerHTML = 'Hello World';
-    $('#simple-main').text( 'Hello, JQuery');
+    $('#simple-main').text( 'Hello, MDBootstrap');
 });
--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,13 +1,15 @@
-// JQuery
-require('./import-jquery');
+// // JQuery
+// const jquery = require('jquery/dist/jquery.js');
+// window.$ = window.jQuery = jquery;
+import './import-jquery';
 
 // Bootstrap
-require('bootstrap/dist/js/bootstrap.js');
-require('bootstrap/dist/css/bootstrap.css');
+import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/css/bootstrap.css';
 
 // MDBootstrap
-require('mdbootstrap/js/mdb.js');
-require('mdbootstrap/css/mdb.css');
+import 'mdbootstrap/js/mdb.js';
+import 'mdbootstrap/css/mdb.css';
 
 // App
-require('./app');
+import './app';
